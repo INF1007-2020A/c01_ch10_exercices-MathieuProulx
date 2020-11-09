01b327e6c6e8ab1c4605c2133b2ee4e80fdc5eda
Mathieu Proulx <mathieu.proulx@polymtl.ca>
2020-11-09 14:15:20 -0500

    #1,2,3 done

--- exercice.py
+++ exercice.py
@@ -40,15 +40,3 @@ if __name__ == '__main__':
 
 
 
-#1. Créer un array présentant 64 valeurs uniformément réparties entre -1.3 et 2.5.
-
-#2. Créer une fonction qui convertit une liste de coordonnées cartésiennes (x, y) 
-#en coordonnées polaires (rayon, angle).
-
-#3. Créer un programme qui trouve l’index de la valeur la plus proche d’un nombre 
-#fournit dans un array.
-
-#4. Créer un graphe de y=𝑥^2  sin⁡(1∕𝑥^2 )+𝑥 dans l’intervalle [-1, 1] avec 250 points.
-
-#5. Évaluer l’intégrale ∫_(−∞)^∞ 𝑒^(−𝑥^2) 𝑑𝑥. Afficher dans un graphique ∫𝑒^(−𝑥^2) 𝑑𝑥 
-#pour x = [-4, 4].
